60c9ef852f5056a4c14050855992a2390ddf8463
Sidharthk002 <sidharthk002@gmail.com>
2023-06-22 21:57:49 +0530

    Created Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import "./Nav.css";
+function Nav() {
+
+  return (
+    <section className="nav">
+      <h1 className="title">Sidharth Kumar</h1>
+      <ul className="ul">
+        <li className="list">Home</li>
+        <li className="list">About</li>
+        <li className="list">Skills</li>
+        <li className="list">Projects</li>
+        <li className="list">Contact</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Nav;

40b05306cb063de77e3694bc318ebf03481cbc67
Sidharthk002 <sidharthk002@gmail.com>
2023-06-23 14:47:28 +0530

    nav change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,86 @@
-import "./Nav.css";
-function Nav() {
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Home', 'About', 'Skills', 'Projects', 'Contact'];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <section className="nav">
-      <h1 className="title">Sidharth Kumar</h1>
-      <ul className="ul">
-        <li className="list">Home</li>
-        <li className="list">About</li>
-        <li className="list">Skills</li>
-        <li className="list">Projects</li>
-        <li className="list">Contact</li>
-      </ul>
-    </section>
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={{base:'8px',md:'35px'}}>
+        <Flex h={'70px'} alignItems={'center'} justifyContent={'space-between'}>
+        <Flex alignItems={'center'}>
+           <Text fontFamily={"Satisfy, cursive"} fontSize={{base:'20px',md:'35px'}} marginLeft={{base:'30px'}} fontWeight={'semibold'}>Sidharth Kumar</Text>
+          </Flex>
+          <IconButton
+            size={'lg'}
+            fontSize={"28px"}
+            icon={isOpen ? <CloseIcon  fontSize={"20px"}/> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+            marginLeft={{base:'40px',md:"40px"}}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}
+              fontSize={'17px'}
+              fontWeight={'medium'}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }} >
+            <Stack as={'nav'} spacing={4} >
+              {Links.map((link) => (
+                <NavLink key={link} >{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4}>Main Content Here</Box>
+    </>
   );
 }
-
-export default Nav;
